57df862a1541ba99374f198e2132409170c16892
Amar Dongardive <amardongardive8@gmail.com>
2025-09-08 10:28:56 +0530

    Update WeatherForecastController.cs

--- TestAPI_pipeline/Controllers/WeatherForecastController.cs
+++ TestAPI_pipeline/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestAPI_pipeline.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing_test", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
