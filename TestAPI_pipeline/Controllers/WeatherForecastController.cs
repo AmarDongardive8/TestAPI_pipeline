f4f077abd4f112813cec998a3be6eaecc0021017
Amar Dongardive <amardongardive8@gmail.com>
2025-09-08 10:44:31 +0530

    Update WeatherForecastController.cs

--- TestAPI_pipeline/Controllers/WeatherForecastController.cs
+++ TestAPI_pipeline/Controllers/WeatherForecastController.cs
@@ -18,8 +18,19 @@ namespace TestAPI_pipeline.Controllers
             _logger = logger;
         }
 
-        [HttpGet("GetWeatherForecast_test")]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("GetWeatherForecast_testing")]
+        public IEnumerable<WeatherForecast> Get1()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
