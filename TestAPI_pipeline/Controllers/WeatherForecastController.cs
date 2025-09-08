0192db763cd9b973f531282dba11cd3d13b4ee1e
Amar Dongardive <amardongardive8@gmail.com>
2025-09-08 10:43:30 +0530

    Update WeatherForecastController.cs

--- TestAPI_pipeline/Controllers/WeatherForecastController.cs
+++ TestAPI_pipeline/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace TestAPI_pipeline.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast_test")]
+        [HttpGet("GetWeatherForecast_test")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
